e5cec5521b209efad08e13bdfc89e27e6323a4ea
Leonardo Simi <leonardo.simi@yahoo.it>
2021-05-26 18:56:19 +0200

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import './screens/products_overview_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MyShop',
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+        accentColor: Colors.lime,
+        fontFamily: 'Lato',
+      ),
+      home: ProductsOverviewScreen(),
+    );
+  }
+}
+// DA VEDERE 8-6
