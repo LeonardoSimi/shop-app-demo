871bf48dd46887319c19e7698ed44e13899db480
Leonardo Simi <leonardo.simi@yahoo.it>
2021-06-02 15:20:34 +0200

    fix cart

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -1,16 +1,61 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../widgets/products_grid.dart';
+import '../widgets/badge.dart';
+import '../providers/cart.dart';
 
-class ProductsOverviewScreen extends StatelessWidget {
+enum FilterOptions {
+  Favorites,
+  All,
+}
 
+class ProductsOverviewScreen extends StatefulWidget {
+  @override
+  _ProductsOverviewScreenState createState() => _ProductsOverviewScreenState();
+}
+
+class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
+  var _showOnlyFavorites = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: Text('myShop'),
+          actions: <Widget>[
+            PopupMenuButton(
+              onSelected: (FilterOptions selectedValue) {
+                setState(() {
+                  if (selectedValue == FilterOptions.Favorites)
+                  {
+                    _showOnlyFavorites = true;
+                  }
+                  else
+                  {
+                    _showOnlyFavorites = false;
+                  }
+                });
+              },
+              itemBuilder: (_) => [
+                PopupMenuItem(
+                    child: const Text('Only Favorites'),
+                    value: FilterOptions.Favorites),
+                PopupMenuItem(
+                    child: const Text('Show All'), value: FilterOptions.All),
+              ],
+              icon: Icon(Icons.more_vert),
+            ),
+            Consumer<Cart>(
+                builder: (_, cart, ch) => Badge(
+                  child: ch!,
+                  value: cart.itemCount != null ? cart.itemCount.toString() : '0',
+                  ),
+                child:
+                IconButton(icon: Icon(Icons.shopping_cart,),
+                onPressed: (){},),
+                ),
+          ],
         ),
-        body: ProductsGrid());
+        body: ProductsGrid(_showOnlyFavorites));
   }
 }
-
